973ed4914edf4c3d4bb3b3405a3a6bd9f4151f7b
Jackson <Jacksonf955@gmail.com>
2019-09-12 18:17:30 -0400

    base for rest of the program

--- paint.py
+++ paint.py
@@ -1,56 +1,65 @@
 import pygame
 from pygame.locals import *
-import tkinter
-import sys, argparse
-
-class Window(tkinter.Frame):
-    def __init__(self,master=None):
-        super().__init__(master)
-        self.master = master
-        self.init_window()
-
-    def init_window(self):
-        self.master.title = "Window 1"
-        self.pack(fill=tkinter.BOTH, expand=1)
-
-        #quit button
-        quitButton = tkinter.Button(self, text="Quit",command=self.window_exit)
-        quitButton.place(x=0, y=0)
-    
-    def window_exit(self):
-        sys.exit()
-
-def get_event():
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+import tkinter as tk
+import sys, argparse, os
+from random import *
 
 def main():
-    pygame.init()
+    root = tk.Tk()
     parser = argparse.ArgumentParser()
-    
+    clock = pygame.time.Clock()
+
+    #variables
+    image = None #image to edit
+    brushColor = (0,0,0)
+    brushRadius = 5
+
     #system arguments
     parser.add_argument('--image', help='Image to view or edit')
     parser.add_argument('--w', help='Image Width')
     parser.add_argument('--h', help='Image Height')
     args = parser.parse_args()
 
-    if args.image == None:
+    #tkinter stuff
+    text = tk.Button(root, text='garbage')
+    text.pack()
+
+    if args.w != None and args.h != None:
         screenSize = (int(args.w),int(args.h))
+    elif args.image != None:
+        image = pygame.image.load(str(args.image))
+        screenSize = image.get_rect().size
     else:
-        pass #get image resolution and set screen to that
+        screenSize = (800,600)
 
-    screen = pygame.display.set_mode(screenSize)
+    embed = tk.Frame(root,width=screenSize[0], height=screenSize[1])
+    embed.pack()
+    os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+    root.update()
 
-    #tkinker
-    root = tkinter.Tk()
-    root.geometry("400x300")
-    app = Window(master=root)
-    app.mainloop()
+    #pygame
+    pygame.display.init()
+    screen = pygame.display.set_mode((screenSize))
+
+    if image == None:
+        screen.fill((255,255,255))
+    else:
+        screen.blit(image,(0,0))
 
     while True:
-        get_event()
-        screen.fill((0,0,0))
-        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                pygame.draw.circle(screen,brushColor,pos,brushRadius)
 
+        print(pygame.mouse.get_pos())
+
+        #rx, ry = randint(0,screenSize[0]), randint(0,screenSize[1])
+        #pygame.draw.circle(screen,(255,0,0),(rx,ry),30)
+
+        root.update()
+        pygame.display.update()
+    
 main()
