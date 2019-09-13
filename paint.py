cdd987c5d72536446c3ca63ea4e3a6598d9be257
JacksonGuy <jacksonf955@gmail.com>
2019-09-12 21:51:48 -0400

    added brush tool and color selection

--- paint.py
+++ paint.py
@@ -4,62 +4,105 @@ import tkinter as tk
 import sys, argparse, os
 from random import *
 
-def main():
-    root = tk.Tk()
-    parser = argparse.ArgumentParser()
-    clock = pygame.time.Clock()
+def test_function(event):
+    global brushColor
+    pygame.draw.circle(screen,(brushColor),(event.x,event.y),brushRadius)
 
-    #variables
-    image = None #image to edit
-    brushColor = (0,0,0)
-    brushRadius = 5
+def set_rgb_color():
+    global brushColor
+    r = app1.r_entry.get()
+    g = app1.g_entry.get()
+    b = app1.b_entry.get()
+    brushColor = (int(r),int(g),int(b))
 
-    #system arguments
-    parser.add_argument('--image', help='Image to view or edit')
-    parser.add_argument('--w', help='Image Width')
-    parser.add_argument('--h', help='Image Height')
-    args = parser.parse_args()
+class toolsApp:
+    def __init__(self,master):
+        self.master = master
 
-    #tkinter stuff
-    text = tk.Button(root, text='garbage')
-    text.pack()
+        self.button1 = tk.Button(self.master,text='Brush',command=set_rgb_color).grid(row=0,column=0)
 
-    if args.w != None and args.h != None:
-        screenSize = (int(args.w),int(args.h))
-    elif args.image != None:
-        image = pygame.image.load(str(args.image))
-        screenSize = image.get_rect().size
-    else:
-        screenSize = (800,600)
+        self.r_text = tk.Label(self.master, text="Red Value")
+        self.r_text.grid(row=1,column=0)
+        self.r_entry = tk.Entry(self.master)
+        self.r_entry.grid(row=1,column=1)
+        
+        self.g_text = tk.Label(self.master, text="Green Value")
+        self.g_text.grid(row=2,column=0)
+        self.g_entry = tk.Entry(self.master)
+        self.g_entry.grid(row=2,column=1)
+        
+        self.b_text = tk.Label(self.master, text="Blue Value")
+        self.b_text.grid(row=3,column=0)
+        self.b_entry = tk.Entry(self.master)
+        self.b_entry.grid(row=3,column=1)
 
-    embed = tk.Frame(root,width=screenSize[0], height=screenSize[1])
-    embed.pack()
-    os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+#windows
+root = tk.Tk()
+toolWindow = tk.Toplevel()
+
+parser = argparse.ArgumentParser()
+clock = pygame.time.Clock()
+
+#variables
+image = None #image to edit
+global brushColor
+brushColor = (0,0,0)
+brushRadius = 5
+
+#system arguments
+parser.add_argument('--image', help='Image to view or edit')
+parser.add_argument('--w', help='Image Width')
+parser.add_argument('--h', help='Image Height')
+args = parser.parse_args()
+
+#tkinter stuff
+#menu
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+
+filesMenu = tk.Menu(menubar,tearoff=0)
+filesMenu.add_command(label="Open")
+filesMenu.add_command(label="Save")
+editMenu = tk.Menu(menubar,tearoff=0)
+editMenu.add_command(label="Copy")
+editMenu.add_command(label="Paste")
+editMenu.add_command(label="Cut")
+menubar.add_cascade(label="File", menu=filesMenu)
+menubar.add_cascade(label="Edit", menu=editMenu)
+
+#applications
+app1 = toolsApp(toolWindow)
+
+if args.w != None and args.h != None:
+    screenSize = (int(args.w),int(args.h))
+elif args.image != None:
+    image = pygame.image.load(str(args.image))
+    screenSize = image.get_rect().size
+else:
+    screenSize = (800,600)
+
+embed = tk.Frame(root,width=screenSize[0], height=screenSize[1])
+embed.pack()
+os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+root.update()
+
+#pygame
+pygame.display.init()
+screen = pygame.display.set_mode(screenSize)
+
+#keyboard bindings
+embed.bind("<B1-Motion>",test_function)
+
+if image == None:
+    screen.fill((255,255,255))
+else:
+    screen.blit(image,(0,0))
+
+while True:
+    #print(pygame.mouse.get_pos())
+    #rx, ry = randint(0,screenSize[0]), randint(0,screenSize[1])
+    #pygame.draw.circle(screen,(255,0,0),(rx,ry),30)
+
+    pygame.display.update()
     root.update()
 
-    #pygame
-    pygame.display.init()
-    screen = pygame.display.set_mode((screenSize))
-
-    if image == None:
-        screen.fill((255,255,255))
-    else:
-        screen.blit(image,(0,0))
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                pygame.draw.circle(screen,brushColor,pos,brushRadius)
-
-        print(pygame.mouse.get_pos())
-
-        #rx, ry = randint(0,screenSize[0]), randint(0,screenSize[1])
-        #pygame.draw.circle(screen,(255,0,0),(rx,ry),30)
-
-        root.update()
-        pygame.display.update()
-    
-main()
